e7becf5d4aa8ae29dd349924a71709b734257d7e
George <george.dimulski@gmail.com>
2018-01-18 23:56:46 +0200

    Add header, footer, top-ten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/app/app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FeaturedComponent } from './components/featured/featured.component';
+import { TopTenComponent } from './components/top-ten/top-ten.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    TopTenComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
