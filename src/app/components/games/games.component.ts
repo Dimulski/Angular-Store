66006f43077cbd1c1dc6fce2933dafad71cf7e76
George <george.dimulski@gmail.com>
2019-02-18 21:18:57 +0200

    Implement genres

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -9,11 +9,29 @@ import { AngularFirestore } from 'angularfire2/firestore';
 export class GamesComponent implements OnInit {
 
   games: any[];
+  genres: string[];
+  genresGames: any[];
+  p: { [id: string]: number} = {};
 
   constructor(db: AngularFirestore) {
     db.collection('/games').valueChanges().subscribe((games) => {
       this.games = games;
-    })
+    });
+
+    this.genres = [];
+    this.genresGames = [];
+    db.collection('/genres').snapshotChanges().subscribe((genres) => {
+      genres.forEach(genre => {
+        let genreName = genre.payload.doc.id.replace(/\s+/, '-');
+        this.genres.push(genreName);
+        if (this.genresGames[genreName] == undefined) {
+          this.genresGames[genreName] = [];
+        }
+        this.genresGames[genreName].push(this.games.filter(game => {
+          return game.genres.includes(genreName.replace(/-/, ' '));
+        }));
+      });
+    });
   }
 
   ngOnInit() { }
