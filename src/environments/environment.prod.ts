df850b15a49e5609f18923af5af3cc44b1586226
George <george.dimulski@gmail.com>
2019-03-12 23:25:52 +0200

    deploy Angular-Store

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWew7VF7GDH1LA-J9qwSCnANSZjhLDaXE",
+    authDomain: "angular-store-f3cbb.firebaseapp.com",
+    databaseURL: "https://angular-store-f3cbb.firebaseio.com",
+    projectId: "angular-store-f3cbb",
+    storageBucket: "angular-store-f3cbb.appspot.com",
+    messagingSenderId: "101457395667"
+  }
 };
